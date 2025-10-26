cbff746391c3a33f5554c03b3cedf89511a785f2
Gabriella <gabriella.parra05@gmail.com>
2025-10-26 19:31:25 -0300

    pastas app, home e pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import Footer from './componets/footer/footer'
-import Navbar from './componets/navbar/navbar'
-import Home from './pages/home/Home'
+import Footer from "./componets/footer/footer";
+import Navbar from "./componets/navbar/navbar";
+import Cadastro from "./pages/cadastro/cadastro";
+import Home from "./pages/home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
 
 
 function App() {
-	return (
-		<>
-			<Navbar />
-			<Home />
-			<Footer />
-		</>
-	)
+    return (
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <div className="min-h-[80vh]">
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/cadastro" element={<Cadastro />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+export default App;

7c91b9b137328f9a94a65eb5a63cb20c4f460497
Gabriella <gabriella.parra05@gmail.com>
2025-10-27 23:12:07 -0300

    cadstro/usuario/context

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import Footer from "./componets/footer/footer";
-import Navbar from "./componets/navbar/navbar";
-import Cadastro from "./pages/cadastro/cadastro";
-import Home from "./pages/home/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/login/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+
+import { AuthProvider } from "./contexts/AuthContext";
+
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Navbar from "./componets/navbar/navbar";
+import Footer from "./componets/footer/footer";
+import Cadastro from "./pages/cadastro/cadastro";
 
 function App() {
     return (
         <>
-            <BrowserRouter>
-                <Navbar />
-                <div className="min-h-[80vh]">
-                    <Routes>
-                        <Route path="/" element={<Login />} />
-                        <Route path="/home" element={<Home />} />
-                        <Route path="/cadastro" element={<Cadastro />} />
-                    </Routes>
-                </div>
-                <Footer />
-            </BrowserRouter>
+            <AuthProvider>
+                <BrowserRouter>
+                    <Navbar />
+                    <div className="min-h-[80vh]">
+                        <Routes>
+                            <Route path="/" element={<Login />} />
+                            <Route path="/home" element={<Home />} />
+                            <Route path="/cadastro" element={<Cadastro />} />
+                        </Routes>
+                    </div>
+                    <Footer />
+                </BrowserRouter>
+            </AuthProvider>
         </>
     );
 }
